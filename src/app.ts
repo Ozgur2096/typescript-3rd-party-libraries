5f03911f0fe81a15ef43bc69d20148676dc16206
Ozgur Ozhan <ozgur2096@gmail.com>
2023-11-14 17:49:42 +0100

    module completed

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import { Product } from './product.model';
 
@@ -29,3 +30,14 @@ const loadedProducts = plainToClass(Product, products);
 for (const product of loadedProducts) {
   console.log(product.getInformation());
 }
+
+// Class Validator
+const product1 = new Product('', -5.99);
+validate(product1).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION FAILED');
+    console.log(errors);
+  } else {
+    console.log(product1.getInformation());
+  }
+});
