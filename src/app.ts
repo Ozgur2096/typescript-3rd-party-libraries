335e8b4a12d5176251eb05d905c53135de280581
Ozgur Ozhan <ozgur2096@gmail.com>
2023-11-14 17:14:34 +0100

    class-transformator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { Product } from './product.model';
+
 import _ from 'lodash';
 
 declare var GLOBAL: any;
@@ -5,3 +7,13 @@ declare var GLOBAL: any;
 console.log(_.shuffle([1, 2, 3]));
 
 console.log(GLOBAL);
+
+// No Types Needed: class-transformer
+
+const products = [
+  { title: 'A car', price: 1000 },
+  { title: 'A computer', price: 800 },
+];
+
+const product1 = new Product('book', 12.99);
+console.log(product1.getInformation());
